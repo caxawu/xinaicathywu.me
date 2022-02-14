3d98136dc4a8df08d0d6656ae795cd915baff2c6
Cathy Wu <caxawu@gmail.com>
2022-02-14 18:34:52 -0500

    routing

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,44 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
+import {
+  BrowserRouter as Router, Routes, Route, NavLink,
+} from 'react-router-dom';
+import '../style.scss';
+import About from './about';
+import Other from './other';
+import Portfolio from './portfolio';
+import Art from './art';
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
+const Nav = (props) => {
+  return (
+    <ul>
+      <li><NavLink to="/">about</NavLink></li>
+      <li><NavLink to="/portfolio">portfolio</NavLink></li>
+      <li><NavLink to="/other-works">other works</NavLink></li>
+      <li><NavLink to="/art">art</NavLink></li>
+    </ul>
+  );
+};
 
-    this.state = {
-    };
-  }
-
-  render() {
-    return (
+const Header = () => {
+  return (
+    <Router>
+      <NavLink exact to="/"><div className="logo" /></NavLink>
       <div className="header">
         <div className="logo">Xinai (Cathy) Wu</div>
         <div className="nav">
-          <button type="button" id="#about-button" onClick={() => this.props.handlePageChange('about')}>about</button>
-          <button type="button" id="#portfolio-button" onClick={() => this.props.handlePageChange('portfolio')}>portfolio</button>
-          <button type="button" id="#other-button" onClick={() => this.props.handlePageChange('other')}>other works</button>
-          <button type="button" id="#art-button" onClick={() => this.props.handlePageChange('art')}>studio & digital art</button>
+          <Nav />
         </div>
       </div>
-    );
-  }
-}
+      <Routes>
+        <Route exact path="/" element={About} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/other-works" element={<Other />} />
+        <Route path="/art" element={<Art />} />
+        <Route render={() => (<div>Page not found </div>)} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -3,64 +3,22 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
 import Header from './components/header';
-import About from './components/about';
-import Other from './components/other';
-import Portfolio from './components/portfolio';
-import Art from './components/art';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      page: 'about',
     };
   }
 
   componentDidMount() {
   }
 
-  handlePageChange = (newPage) => {
-    this.setState({ page: newPage });
-  }
-
-  renderPage() {
-    if (this.state.page === 'art') {
-      return (
-        <div>
-          <div className="display">art</div>
-          <Art />
-        </div>
-      );
-    } else if (this.state.page === 'portfolio') {
-      return (
-        <div>
-          <div className="display">portfolio</div>
-          <Portfolio />
-        </div>
-      );
-    } else if (this.state.page === 'other') {
-      return (
-        <div>
-          <div className="display">other</div>
-          <Other />
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <div className="display">about</div>
-          <About />
-        </div>
-      );
-    }
-  }
-
   render() {
     return (
       <div>
-        <Header handlePageChange={this.handlePageChange} />;
-        {this.renderPage()}
+        <Header />
       </div>
     );
   }
